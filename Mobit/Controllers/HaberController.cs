c9638cec92bfa7787d53db98cc434d39f6c3418a
mustafae <mustafae@DESKTOP-8DK0DA6>
2017-06-01 03:50:21 +0300

    dökümanlar

---
+++ Mobit/Controllers/HaberController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mobit.Controllers
+{
+    public class HaberController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
