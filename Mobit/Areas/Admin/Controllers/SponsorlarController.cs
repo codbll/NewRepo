c8f48eba5638427c76af25fdd7807e475f5dfddd
mustafae <mustafae@DESKTOP-8DK0DA6>
2017-09-21 09:53:20 +0300

    mustafae

--- Mobit/Areas/Admin/Controllers/SponsorlarController.cs
+++ Mobit/Areas/Admin/Controllers/SponsorlarController.cs
@@ -39,7 +39,7 @@ namespace Mobit.Areas.Admin.Controllers
             var folder = Server.MapPath("~/Upload/slide");
             if (!Directory.Exists(folder))
             {
-                Directory.CreateDirectory(folder);
+                //Directory.CreateDirectory(folder);
             }
 
             if (resimYukle != null)
@@ -96,7 +96,7 @@ namespace Mobit.Areas.Admin.Controllers
             var folder = Server.MapPath("~/Upload/slide");
             if (!Directory.Exists(folder))
             {
-                Directory.CreateDirectory(folder);
+                //Directory.CreateDirectory(folder);
             }
 
             if (resimYukle != null)
